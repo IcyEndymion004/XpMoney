a6fad57ca684fceb6ea3eeec8d3fa59aa8947b0b
Soulz <84856878+Pinesolz@users.noreply.github.com>
2021-08-31 15:50:34 -0700

    Thanks to Icy for fixing errors

--- src/Soulz/XpMoney/Loader.php
+++ src/Soulz/XpMoney/Loader.php
@@ -4,8 +4,9 @@ namespace Soulz\XpMoney;
 
 use onebone\economyapi\EconomyAPI;
 
-use libs\jojoe77777\FormAPI\CustomForm;
-use libs\jojoe77777\FormAPI\SimpleForm;
+use jojoe77777\FormAPI\CustomForm;
+use jojoe77777\FormAPI\SimpleForm;
+use jojoe77777\FormAPI;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
@@ -16,115 +17,104 @@ use pocketmine\Player;
 
 use pocketmine\plugin\PluginBase;
 
-use pocketmine\scheduler\ClosureTask;
-use pocketmine\scheduler\TaskScheduler;
 
 use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
 
 class Loader extends PluginBase implements Listener {
 
+    public $plugin;
+
     public const ALERT = TextFormat::DARK_GRAY . "[" . TextFormat::RED . "!" . TextFormat::DARK_GRAY . "]" . TextFormat::GRAY;
 
     public const SUCCESS = TextFormat::DARK_GRAY . "[" . TextFormat::GREEN . "!" . TextFormat::DARK_GRAY . "]" . TextFormat::GRAY;
 
-    private function getScheduler(): TaskScheduler {
-        return Loader::getInstance()->getScheduler();
-    }
-
-    public function sendForm(Player $player, $form): void {
-        $this->getScheduler()->scheduleDelayedTask(new ClosureTask(function() use ($player, $form): void {
-            $player->sendForm($form);
-        }), 5);
-    }
 
     public function onEnable(){
-        $this->getServer()->registerEvents($this, $this);
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
     /**
      * @param CommandSender $sender
      * @param Command $command
-     * string $label
-     * @param array $args
-     * @return bool
      */
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool 
     {
         if($command->getName() == "xpmoney"){
             if(!$sender instanceof Player){
                 $sender->sendMessage(Loader::ALERT . "You must execute this command in-game");
-                return;
             }
             $exp = $sender->getCurrentTotalXp();
-            $form = new SimpleForm(function(Player $player, $data): void {
+            $form = new SimpleForm(function(Player $sender, $data): void {
                 if($data !== null){
                     switch($data){
                         case "xpToMoney":
-                            $this->sendXtmForm($player);
-                            break;
-                        case "moneyToXp":
-                            $this->sendMtxForm($player);
-                            break;
+                            case "xpToMoney":
+                                $this->sendXtmForm($sender);
+                                break;
+                            case "moneyToXp":
+                                $this->sendMtxForm($sender);
+                                break;
                 }
             }
         });
-            $eco = EconomyAPI::getInstance();
-            $money = $eco->myMoney($player);
+            $economy = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI")->getInstance();
+            $money = $economy->myMoney($sender);
 
             // Base Simple Form
             $form->setTitle("Transfer Economies");
             $form->addButton(TextFormat::AQUA . "EXP => Money\n", -1, "", "xpToMoney");
             $form->addButton(TextFormat::AQUA . "Money => EXP\n", -1, "", "moneyToXp");
             $form->setContent(TextFormat::GRAY . "Choose How You'd Like to Transfer\n" . TextFormat::AQUA . "Your EXP: " . number_format($exp) . "\n" . TextFormat::AQUA . "Your Money: " . $money);
-            $this->sendForm($sender, $form);
+            $sender->sendForm($form);
           
         }
+    return true;
     }
 
     // XP => Money Custom Form
     public function sendXtmForm(Player $player): void {
-        $eco = EconomyAPI::getInstance();
         $form = new CustomForm(function(Player $player, $data): void {
+            $economy = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI")->getInstance();
             if($data !== null){
                 $amount = intval($data["amount"]);
                 $cost = $this->getConfig()->get("xp-per-money-tick") * $amount;
                 if($player->getCurrentTotalXp() - $cost < 0) {
                     $player->sendMessage(Loader::ALERT . "You do not have enough EXP for this action! " . TextFormat::GOLD . "EXP Needed: " . $cost);
                 } else {
-                    $eco->addMoney($player, $amount);
+                    $economy->addMoney($player, $amount);
                     $player->subtractXp($cost);
-                    $player->sendMessage(Loader::SUCCESS . "You have bought "  . TextFormat::AQUA . "$ $amount " . TextFormat::GRAY . " for " . TextFormat::AQUA . $cost . TextFormat::GRAY . " XP!");
+                    $player->sendMessage(Loader::SUCCESS . "You have bought "  . TextFormat::AQUA . "$amount Exp" . TextFormat::GRAY . " for " . TextFormat::AQUA . $cost . TextFormat::GRAY . " XP!");
                 }
                 return;
             }
         });
         $form->setTitle(TextFormat::BOLD . TextFormat::AQUA . "EXP => Money");
-        $form->addLabel("§7EXP: $data §6Per: $1");
+        $form->addLabel("§eEXP: 1 §6Per: $1");
         $form->addSlider("Amount" , 1, 64, 1, 1, "amount");
-        $this->sendForm($player, $form);
+        $player->sendForm($form);
     }
 
     // Money => XP Custom Form
     public function sendMtxForm(Player $player): void {
-        $eco = EconomyAPI::getInstance();
         $form = new CustomForm(function(Player $player, $data): void {
+            $economy = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI")->getInstance();
             if($data !== null){
                 $amount = intval($data["amount"]);
                 $cost = $this->getConfig()->get("money-per-xp-tick") * $amount;
-                if($player->getMoney() - $cost < 0) {
+                if($economy->myMoney($player) < 0) {
                     $player->sendMessage(Loader::ALERT . "You do not have enough money for this action! " . TextFormat::GOLD . "Money Needed: " . $cost);
                 } else {
-                    $player->addtXp($amount);
-                    $eco->reduceMoney($player, $amount);
+                    $player->addXp($amount);
+                    $economy->reduceMoney($player, $amount);
                     $player->sendMessage(Loader::SUCCESS . "You have bought " . TextFormat::AQUA . " $amount " . TextFormat::GRAY . " EXP for " . TextFormat::AQUA . $cost . TextFormat::GRAY . " Money!");
                 }
                 return;
             }
         });
         $form->setTitle(TextFormat::BOLD . TextFormat::AQUA . "Money => Exp");
-        $form->addLabel("§7Money: $data §6Per: 1 EXP");
+        $form->addLabel("§6Money: 1$ §ePer: 1 EXP");
         $form->addSlider("Amount" , 1, 64, 1, 1, "amount");
-        $this->sendForm($player, $form);
+        $player->sendForm($form);
     }
 }
